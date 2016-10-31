47dc3e05c7d01aa113160c55936d58f778854e18
Christopher Syers <csyers@nd.edu>
2016-10-31 12:41:37 -0400

    basic make functionality and dummy programs

---
+++ server/myfrmd.c
@@ -0,0 +1,12 @@
+/* Name: Tim Chang (tchang2), Christopher Syers (csyers)
+ * Date: November 1, 2016
+ * CSE 30264: Computer Networks
+ * Programming Assignment 4: Program a Prototype of a Message Board Forum application
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    printf("hello world\n");
+    return 0;
+}
