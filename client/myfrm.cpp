1cfef56384c60a300df575131aa19a33ae6ee0f1
Christopher Syers <csyers@nd.edu>
2016-11-11 10:38:08 -0500

    client socket creation

--- client/myfrm.cpp
+++ client/myfrm.cpp
@@ -7,24 +7,56 @@
 #include "util.h"
 
 void print_usage_and_exit();
+void print_error_and_exit(string message);
 
 int main(int argc, char* argv[]){
-    string server, port_temp;
+    string host, port_temp;
     int port;
+    
+    // check the number of arguemnts
     if(argc != 3){
+        // if there are the wrong number of arguments, exit
         print_usage_and_exit();
     } else{
-        server = argv[1];
+        // get the host name and port number
+        host = argv[1];
         port_temp = argv[2];
+        // check if the port only contains digits
         bool has_only_digits = (port_temp.find_first_not_of( "0123456789" ) == string::npos);
         if(has_only_digits) {
+            // set port variable
             port = atoi(port_temp.c_str());
         } else{
-            cerr << "myfrm: port argument can only contain numbers" << endl;
-            exit(1);
+            // print error if there are non-digits in the port argument
+            print_error_and_exit("port argument must be positive integer");
         }
     }
 
-    cout << "port: " << port << "\tserver: " << server << endl;
+    // local variables for socket programming
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    int s_udp, s_tcp;
+
+    // get host name and if it is not resolvable, print error and exit
+    if(!(hp =  gethostbyname(host.c_str()))){
+        print_error_and_exit("unknown host");
+    }
+
+    // setup the sockaddr_in
+    bzero((char *)&sin,sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr,(char *)&sin.sin_addr,hp->h_length);
+    sin.sin_port = port;
+
+    // open a UDP socket, exit if there is an error
+    if((s_udp=socket(PF_INET,SOCK_DGRAM,0))<0){
+        print_error_and_exit("could not open UDP socket");
+    }
+
+    // open a TCP socket, exit if there is an error
+    if((s_tcp=socket(PF_INET,SOCK_STREAM,0))<0){
+        print_error_and_exit("could not open TCP socket");
+    }
+
     return 0;
 }
---
+++ client/util.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <netdb.h>
+using namespace std;
+
+void print_usage_and_exit(){
+    cout << "usage: myfrm server port" << endl;
+    exit(1);
+}
+
+void print_error_and_exit(string message){
+    cerr << "myfrm: " << message << endl;
+}
