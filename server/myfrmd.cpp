e50ca164b8701149170b6dd452ba7dda0efaccc0
Christopher Syers <csyers@nd.edu>
2016-11-10 23:03:12 -0500

    file structure and input checking for server

--- server/myfrmd.cpp
+++ server/myfrmd.cpp
@@ -4,11 +4,30 @@
  * Programming Assignment 4: Program a Prototype of a Message Board Forum application
  */
 
-#include <iostream>
+#include "util.h"
 
 using namespace std;
 
+void print_usage_and_exit();
+
 int main(int argc, char* argv[]){
-    cout << "hello world" << endl;
+    // check usage
+    int port;
+    string port_temp, password;
+    if(argc != 3){
+        print_usage_and_exit();
+    } else{
+        string port_temp = argv[1];
+        password = argv[2];
+        bool has_only_digits = (port_temp.find_first_not_of( "0123456789" ) == string::npos);
+        if(has_only_digits) {
+            port = atoi(argv[1]);
+        } else{
+            cerr << "myfrmd: port argument can only contain numbers" << endl;
+            exit(1);
+        }
+    }
+
+    cout << "port: " << port << "\tpassword: " << password << endl;
     return 0;
 }
---
+++ server/util.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+void print_usage_and_exit(){
+    cout << "usage: myfrmd port password" << endl;
+    exit(1);
+}
